3998ad1c3641ada04da24420ff5cc6ca1b44a7ce
= <=>
2026-01-31 00:14:32 +0100

    fix: refactored FactoryCommand to use Builder class

--- src/main/java/edu/kis/powp/jobs2d/command/FactoryCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/command/FactoryCommand.java
@@ -4,53 +4,71 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FactoryCommand {
+
     public static DriverCommand createHouse() {
         int baseX = -50;
         int baseY = -50;
         int squareSize = 100;
         int roofHeight = 60;
 
-        List<DriverCommand> squareCommands = new ArrayList<>();
-        squareCommands.add(new SetPositionCommand(baseX, baseY));
-        squareCommands.add(new OperateToCommand(baseX + squareSize, baseY));
-        squareCommands.add(new OperateToCommand(baseX + squareSize, baseY + squareSize));
-        squareCommands.add(new OperateToCommand(baseX, baseY + squareSize));
-        squareCommands.add(new OperateToCommand(baseX, baseY));
 
-        CompoundCommand square = CompoundCommand.builder().add(squareCommands).build();
 
-        List<DriverCommand> roofCommands = new ArrayList<>();
-        roofCommands.add(new SetPositionCommand(baseX, -(baseY + squareSize)));
-        roofCommands.add(new OperateToCommand(baseX + squareSize / 2, -(baseY + squareSize + roofHeight)));
-        roofCommands.add(new OperateToCommand(baseX + squareSize, -(baseY + squareSize)));
+        CompoundCommand square = createSquare(baseX, baseY, squareSize);
+
+        CompoundCommand roof = createIsoscelesTriangle(baseX, baseY, squareSize, roofHeight);
 
         return CompoundCommand.builder()
                 .add(square)
-                .add(roofCommands)
+                .add(roof)
                 .setName("House command")
                 .build();
             
     }
 
-    public static DriverCommand createSecretCommand(){
-        List<DriverCommand> commands = new ArrayList<>();
-        commands.add(new SetPositionCommand(-20, -50));
-        commands.add(new OperateToCommand(-20, -50));
-        commands.add(new SetPositionCommand(-20, -40));
-        commands.add(new OperateToCommand(-20, 50));
-        commands.add(new SetPositionCommand(0, -50));
-        commands.add(new OperateToCommand(0, -50));
-        commands.add(new SetPositionCommand(0, -40));
-        commands.add(new OperateToCommand(0, 50));
-        commands.add(new SetPositionCommand(70, -50));
-        commands.add(new OperateToCommand(20, -50));
-        commands.add(new OperateToCommand(20, 0));
-        commands.add(new OperateToCommand(70, 0));
-        commands.add(new OperateToCommand(70, 50));
-        commands.add(new OperateToCommand(20, 50));
+    public static CompoundCommand createSquare(int baseX, int baseY, int squareSize){
         return CompoundCommand.builder()
-                .add(commands)
-                .setName("Top Secret Command")
-                .build();
+        .addSetPosition(baseX,baseY)
+        .addOperateTo(baseX + squareSize, baseY)
+        .addOperateTo(baseX + squareSize, baseY + squareSize)
+        .addOperateTo(baseX, baseY + squareSize)
+        .addOperateTo(baseX, baseY)
+        .build();
+    }
+
+    public static CompoundCommand createIsoscelesTriangle(int baseX, int baseY, int squareSize, int roofHeight){
+        List<DriverCommand> commands = new ArrayList<>();
+        commands.add(new SetPositionCommand(baseX, -(baseY + squareSize)));
+        commands.add(new OperateToCommand(baseX + squareSize / 2, -(baseY + squareSize + roofHeight)));
+        commands.add(new OperateToCommand(baseX + squareSize, -(baseY + squareSize)));
+
+        return CompoundCommand.builder()
+        .add(commands)
+        .addSetPosition(baseX, -(baseY + squareSize))
+        .addOperateTo(baseX + squareSize / 2, -(baseY + squareSize + roofHeight))
+        .addOperateTo(baseX + squareSize, -(baseY + squareSize))
+        .build();
+    }
+
+
+    public static DriverCommand createSecretCommand(){
+
+        return CompoundCommand.builder()
+            .addSetPosition(-20, -50)
+            .addOperateTo(-20, -50)
+            .addSetPosition(-20, -40)
+            .addOperateTo(-20, 50)
+            .addSetPosition(0, -50)
+            .addOperateTo(0, -50)
+            .addSetPosition(0, -40)
+            .addOperateTo(0, 50)
+            .addSetPosition(70, -50)
+            .addOperateTo(20, -50)
+            .addOperateTo(20, 0)
+            .addOperateTo(70, 0)
+            .addOperateTo(70, 50)
+            .addOperateTo(20, 50)
+
+            .setName("Top Secret Command")
+            .build();
     }
 }
